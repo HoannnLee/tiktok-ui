8c5c6f743fb4063b7a368971fb0c79d2df2c8ec7
HoannnLee <Leecghoan@gmail.com>
2025-04-10 11:01:48 +0700

    tạo route layout

---
+++ src/pages/Following/index.js
@@ -0,0 +1,5 @@
+function Following () {
+    return <h2>following Page</h2>
+}
+
+export default Following ;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,5 @@
+function Search () {
+    return <h2>SearchPages</h2>
+}
+
+export default Search ;
